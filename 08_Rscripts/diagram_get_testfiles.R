ef29ea28c3dd45d5b08016ded9017c4c14620d5a
jixing475 <jixing475@jixing475@gmail.comcom>
2018-11-07 08:57:02 +0800

    test bed file

---
+++ 08_Rscripts/diagram_get_testfiles.R
@@ -0,0 +1,30 @@
+
+#流程图
+library(DiagrammeR)
+graph <- 
+  grViz("
+digraph boxes_and_circles {
+# graph attributes
+graph [overlap = true]
+graph [rankdir = LR]
+
+# node attributes
+node [shape = box, //box or circle
+//style = filled,
+fontname = Helvetica,
+penwidth = 2.0,
+color = black ] // frame color
+
+
+# edge attributes
+edge [color = black]
+        
+    # edge statements
+    Bed -> {Bam GTF Gtf} [fillcolor = red]
+    Bam -> {Fastq} [fillcolor = red]
+    Gtf -> Fasta [fillcolor = red]
+                            }
+") 
+graph
+#http://rich-iannone.github.io/DiagrammeR/graphviz_and_mermaid.html#attributes
+ 
